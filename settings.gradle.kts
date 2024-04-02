c4e0b8a52562c88d5647fd54ea2828eb6b3b0290
Rajat <130392640+rajatcantcode@users.noreply.github.com>
2024-04-02 16:52:29 +0530

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "x1_MyFirstKMM"
+include(":androidApp")
+include(":shared")
---
+++ shared/src/androidMain/kotlin/com/example/x1_myfirstkmm/Platform.android.kt
@@ -0,0 +1,7 @@
+package com.example.x1_myfirstkmm
+
+class AndroidPlatform : Platform {
+    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
+}
+
+actual fun getPlatform(): Platform = AndroidPlatform()
